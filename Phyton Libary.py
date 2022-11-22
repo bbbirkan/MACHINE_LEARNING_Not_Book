6dc4b25723a8511d0caf381516fe4412d5a4f65d
bbbirkan <58374229+bbbirkan@users.noreply.github.com>
2022-11-21 21:24:28 -0500

    initial commit

---
+++ Phyton Libary.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+veri = pd.read_csv('veriler.csv')
+boy = veri[['boy']]
+print(boy)
+boykilo = veri[['boy','kilo']]
+print(boykilo)
+
+
+from sklearn.impute import SimpleImputer
+imputer= SimpleImputer(mising_values=np.nan,strategy="mean")
